ae43f6470be1d2ef6691e82f08aa1e23447ca880
Sameer <aijazsameer5@gmail.com>
2022-06-05 14:53:22 -0700

    cvadded

--- components/Hero.js
+++ components/Hero.js
@@ -26,6 +26,9 @@ export default function Hero() {
       </div>
       <h1 className="mt-5 mb-4">{hero.title}</h1>
       <p className="text-lg">{hero.desc}</p>
+      <button type="button" class="py-2 mt-4 px-4  bg-blue-400 hover:bg-blue-100 focus:ring-pink-500 focus:ring-offset-pink-200 text-red-500 w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-full">
+      <a href='/static/sameers_cv.pdf' target="_blank" className='text-dark' download>Download Resume</a>
+</button>
     </div>
   );
 }
